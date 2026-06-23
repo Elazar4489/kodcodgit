94adbc695d8642b443c5abf1fcbce0024c2c8f63
Your Name <emma298450@gmail.com>
2026-06-23 09:15:13 +0300

    Checking whether folder git_kodcode was successfully transferred

--- week11/Monday/Exercise/Program.cs
+++ week11/Monday/Exercise/Program.cs
@@ -7,48 +7,59 @@ namespace ExerciseMonday
         
         static void Main()
         {
+            List<Dictionary<string, string>> data = new List<Dictionary<string, string>>();
             List<int> ids = new List<int>();
             List<string> headings = new List<string>();
             List<int> speeds = new List<int>();
-            AddTrack(300, "hfj", ids, speeds, headings);
-            AddTrack(250, "chaaa", ids, speeds, headings);
-            AddTrack(180, "miau", ids, speeds, headings);
-            Console.WriteLine(GetTrackByID(1, ids, speeds, headings));
-            Console.WriteLine(GetTrackByID(2, ids, speeds, headings));
-            Console.WriteLine(GetTrackByID(3, ids, speeds, headings));
-            Console.WriteLine(DeleteTrack(3, ids, speeds, headings));
-            Console.WriteLine(GetTrackByID(3, ids, speeds, headings));
-            Console.WriteLine(GetAllTracks(ids, speeds, headings));
+            //AddTrack(300, "hfj", ids, speeds, headings);
+            //AddTrack(250, "chaaa", ids, speeds, headings);
+            //AddTrack(180, "miau", ids, speeds, headings);
+            //Console.WriteLine(GetTrackByID(1, ids, speeds, headings));
+            //Console.WriteLine(GetTrackByID(2, ids, speeds, headings));
+            //Console.WriteLine(GetTrackByID(3, ids, speeds, headings));
+            //Console.WriteLine(DeleteTrack(3, ids, speeds, headings));
+            //Console.WriteLine(GetTrackByID(3, ids, speeds, headings));
+            //Console.WriteLine(GetAllTracks(ids, speeds, headings));
         }
-        static bool AddTrack(int speed, string heading, List<int> ids, List<int> speeds, List<string> headings)
+        static bool AddTrack(int speed, string heading, List<Dictionary<string, string>> data)
         {
-            speeds.Add(speed);
-            headings.Add(heading);
-            int lastID = ids.Count == 0 ? 1 : ids[^1] + 1;
-            ids.Add(lastID);
+            int lastid = data.Count == 0 ? 1 : (int)data[^1]["id"] + 1;
+            string lastID = (string)lastid;
+            Dictionary<string, string> track = new Dictionary<string, string>()
+            {
+                {"id", lastID },
+                {"speed", speed },
+                {"heading", heading }
+            }
+            data.Add(track);
+            //speeds.Add(speed);
+            //headings.Add(heading);
+            
+            //ids.Add(lastID);
             return true;
         }
-        static bool DeleteTrack(int id, List<int> ids, List<int> speeds, List<string> headings)
+        static bool DeleteTrack(string id, List<Dictionary<string, string>> data)
         {
-            for (int i = 0; i < ids.Count; i++)
+            for (int i = 0; i < data.Count; i++)
             {
-                if (ids[i] == id)
+                if (data[i]["id"] == id)
                 {
-                    speeds.Remove(speeds[i]);
-                    headings.Remove(headings[i]);
-                    ids.Remove(ids[i]);
+                    data.Remove(data[i])
+                    //speeds.Remove(speeds[i]);
+                    //headings.Remove(headings[i]);
+                    //ids.Remove(ids[i]);
                     return true;
                 }
             }
             return false;
         }
-        static string GetTrackByID(int id, List<int> ids, List<int> speeds, List<string> headings)
+        static string GetTrackByID(string id, List<Dictionary<string, string>> data)
         {
-            for (int i = 0; i < ids.Count; i++)
+            for (int i = 0; i < data.Count; i++)
             {
-                if (ids[i] == id)
+                if (data[i]["id"] == id)
                 {
-                    string TrackByID = $"TrackID: {id}, Heading: {headings[i]}, speed: {speeds[i]}";
+                    string TrackByID = $"TrackID: {id}, Heading: {data[i]["headings"]}, speed: {data[i]["speeds"]}";
                     return TrackByID;
                 }
             }
